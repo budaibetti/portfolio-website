a9311cf181b8d86ec91727042fc784d697192f14
Bettina Budai <budaibetti11@gmail.com>
2024-05-12 21:27:30 +0200

    Adding a work-experience counter

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const likeBtn = document.querySelector(".like-btn");
 const dislikeBtn = document.getElementById("dislike-btn");
 
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
 //typewriter effect
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -111,3 +116,33 @@ function showLangOptions() {
   languageSelector.style.display =
     languageSelector.style.display === "block" ? "none" : "block";
 }
+
+//Work experience counter
+
+const startDay = "12 Sep 2022";
+
+function counter() {
+  const startDate = new Date(startDay);
+  const currentDate = new Date();
+
+  const totalSeconds = new Date(currentDate - startDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.round(totalSeconds / 60) % 60;
+  const seconds = Math.round(totalSeconds % 60);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+
+  console.log(currentDate);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0 ${time}` : time;
+}
+
+setInterval(counter, 1000);
+
+counter();
